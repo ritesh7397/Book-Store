903e76874606b840cba651446a0e6ba98909a86f
Ritesh Nagrikar <riteshnagrikar77@gmail.com>
2025-09-11 00:06:15 +0530

    addes books api

--- BackEnd/routes/book.route.js
+++ BackEnd/routes/book.route.js
@@ -5,15 +5,15 @@ const Book = require("../models/book.model");
 const { authenticateToken } = require("./user.Auth");
 
 
-// ADD BOOK => ADMIN
+// ADD BOOK => THROUGH ADMIN
 router.post("/add-book", authenticateToken, async (req, res) => {
     try {
-        const{id} = req.headers;
+        const { id } = req.headers;
         const user = await User.findById(id);
-        if(user.role !== "admin"){
+        if (user.role !== "admin") {
             return res
                 .status(400)
-                .json({message: "You are not having access to perform admin work"});
+                .json({ message: "You are not having access to perform admin work" });
         }
         const book = new Book({
             url: req.body.url,
@@ -32,16 +32,10 @@ router.post("/add-book", authenticateToken, async (req, res) => {
 module.exports = router;
 
 // UPDATE BOOK
-router.post("/update-book", authenticateToken, async (req, res) => {
+router.put("/update-book", authenticateToken, async (req, res) => {
     try {
-        const{bookid} = req.headers;
-        const user = await User.findById(bookid);
-        if(user.role !== "admin"){
-            return res
-                .status(400)
-                .json({message: "You are not having access to perform admin work"});
-        }
-        const book = new Book({
+        const { bookid } = req.headers;
+        await Book.findByIdAndUpdate(bookid, {
             url: req.body.url,
             title: req.body.title,
             author: req.body.author,
@@ -49,10 +43,62 @@ router.post("/update-book", authenticateToken, async (req, res) => {
             desc: req.body.desc,
             language: req.body.language,
         });
-        await book.save();
-        res.status(200).json({ message: "Book Added successfully" });
+        return res.status(200).json({ message: "Book Updated successfully" });
     } catch (error) {
         res.status(500).json({ message: "Internal server error" });
     }
-})
+});
+
+
+// DELETE BOOK
+router.delete("/delete-book", authenticateToken, async (req, res) => {
+    try {
+        const { bookid } = req.headers;
+        await Book.findByIdAndDelete(bookid);
+        return res.status(200).json({ message: "Book Deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+// GET ALL BOOKS
+router.get("/get-all-books", async (req, res) => {
+    try {
+        const books = await Book.find().sort({ createdAt: -1 });
+        return res.json({
+            status: "Success",
+            data: books,
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+// Get Recently added books
+router.get("/get-recent-books", async (req, res) => {
+    try {
+        const books = await Book.find().sort({ createdAt: -1 }).limit(3);
+        return res.json({
+            status: "Success",
+            data: books,
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+// GET BOOK BY ID
+router.get("/get-book-by-id/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const book = await Book.findById(id);
+        return res.json({
+            status: "Success",
+            data: book,
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
 module.exports = router;
