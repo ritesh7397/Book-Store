0ea3ac0e4fcdf08be21ce1bfaf10fdfaeb2e3840
Ritesh Nagrikar <riteshnagrikar77@gmail.com>
2025-09-25 23:46:27 +0530

    changes in viewbookdeatails

--- FrontEnd/src/components/ViewBookDetails/ViewBookDetails.jsx
+++ FrontEnd/src/components/ViewBookDetails/ViewBookDetails.jsx
@@ -3,11 +3,15 @@ import Loader from '../Loader/Loader';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { GrLanguage } from 'react-icons/gr';
-import { FaHeart, FaShoppingCart } from 'react-icons/fa';
+import { FaHeart, FaShoppingCart,FaEdit, MdOutlineDelete } from 'react-icons/fa';
+import {useSelector} from "react-redux";
 
 const ViewBookDetails = () => {
     const { id } = useParams();
     const [Data, setData] = useState();
+    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+    const role = useSelector((state) => state.auth.role);
+
 
     useEffect(() => {
         const fetch = async () => {
@@ -27,28 +31,48 @@ const ViewBookDetails = () => {
     return (
         <>
         {Data && (
-          <div className='px-4 md:px-12 py-8 bg-zinc-900 flex-col md:flex-row flex gap-8'>
-            <div className='bg-zinc-800 rounded px-4 py-12 w-full lg:w-3/6 flex  justify-around'>
+          <div className='px-4 md:px-12 py-8 bg-zinc-900 flex flex-col lg:flex-row gap-8 items-start'>
+            <div className=' w-full lg:w-3/6 '>
                 {" "}
-               <div>
+               <div className='flex flex-col lg:flex-row justify-around bg-zinc-800 p-12 rounded'>
                 {" "}
                  <img 
                   src={Data.url} 
                   alt={Data.title} 
-                  className='h-[50vh] lg:h-[70vh] rounded'
+                  className='h-[50vh] md:h-[60vh] lg:h-[70vh] rounded'
                  />
-                 <div className='flex md:flex-col'>
-                    <button className='bg-white rounded-full text-3xl p-3 text-red-500'>
-                        <FaHeart/>
+                {
+                 isLoggedIn === true && role === "user" && (
+                     <div className='flex flex-row lg:flex-col items-center justify-between lg:justify-start mt-8 lg:mt-0'>
+                    <button className='bg-white rounded lg:rounded-full text-3xl p-3 text-red-500 flex items-center justify-center'>
+                        <FaHeart/> 
+                        <span className='ms-4 block lg:hidden'>Favourites</span>
                     </button>
-                    <button className='bg-white rounded-full text-3xl p-3 mt-4 text-blue-500'>
-                        <FaShoppingCart/>
+                    <button className='text-white rounded lg:rounded-full text-3xl p-3 mt-0 lg:mt-8 bg-blue-500 flex items-center justify-center'>
+                        <FaShoppingCart/> 
+                        <span className='ms-4 block lg:hidden'>Add to Cart</span>
                     </button>
                  </div>
+                 )}
+                 {
+                   isLoggedIn === true && role === "admin" && (
+                     <div className='flex flex-row lg:flex-col items-center justify-between lg:justify-start mt-8 lg:mt-0'>
+                    <button className='bg-white rounded lg:rounded-full text-3xl p-3  flex items-center justify-center'>
+                        <FaEdit/> 
+                        <span className='ms-4 block lg:hidden'>Edit</span>
+                    </button>
+                    <button className='text-red-500 rounded lg:rounded-full text-3xl p-3 mt-0 lg:mt-8 bg-white flex items-center justify-center'>
+                        <MdOutlineDelete/>
+                         <span className='ms-4 block lg:hidden'>Delete Book</span>
+                    </button>
+                 </div>
+                   )}
                </div>
             </div>
             <div className='p-4 w-full lg:w-3/6'>
-                <h1 className='text-4xl text-zinc-300 font-semibold'>{Data.title}</h1>
+                <h1 className='text-4xl text-zinc-300 font-semibold'>
+                    {Data.title}
+                </h1>
                 <p className='text-zinc-400 mt-1'><strong>by:</strong> {Data.author}</p>
                 <p className='text-zinc-500 mt-4 text-1'><strong>Description:</strong> {Data.desc}</p>
                 <p className='flex mt-4 items-center justify-start text-zinc-400'>
